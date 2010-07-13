aefd21b5929363157bb9f9e1b086f03fbe5e6357
stefan@ott.net <stefan@ott.net@2daba630-7245-0410-8157-d91beb716303>
2010-07-13 02:13:12 +0000

    Moved blocklist code to blocklist.c

--- trunk/src/pidgin-pp.c
+++ trunk/src/pidgin-pp.c
@@ -42,9 +42,10 @@
 //#include <gtkprefs.h>
 
 // our auto-reply functionality
+#include "pp-prefs.h"
 #include "auto-reply.h"
 #include "botcheck.h"
-#include "pp-prefs.h"
+#include "blocklist.h"
 
 #ifdef WIN32
 #include "win32dep.h"
@@ -56,65 +57,6 @@
 #define _(String) ((const char *) (String))
 #define N_(String) ((const char *) (String))
 #endif // ENABLE NLS
-
-static void
-add_to_block_list(const gchar *name)
-{
-	// abort when a category was clicked
-	if (!name)
-	{
-		purple_debug_info("pidgin-pp", "Not blocking (null)\n");
-		return;
-	}
-
-	purple_debug_info("pidgin-pp", "Adding %s to block list\n", name);
-
-	GList* blocklist = prefs_get_block_list();
-	blocklist = g_list_append(blocklist, (gpointer) name);
-	prefs_set_block_list(blocklist);
-}
-
-static void
-remove_from_block_list(const gchar *name)
-{
-	purple_debug(PURPLE_DEBUG_INFO, "pidgin-pp",
-			"Removing %s from block list\n", name);
-
-	GList* blocklist = prefs_get_block_list();
-	GList* tmp = blocklist;
-	while (tmp)
-	{
-		if (!strcmp(tmp->data, name))
-		{
-			blocklist = g_list_delete_link(blocklist, tmp);
-			break;
-		}
-		tmp = g_list_next(tmp);
-	}
-	prefs_set_block_list(blocklist);
-}
-
-static gboolean
-contact_is_blocked(const gchar *name)
-{
-	GList* blocklist = prefs_get_block_list();
-
-	// we don't care about what's after the slash
-	gchar *clean_name = strtok((gchar *) name, "/");
-
-	// abort when a category was clicked
-	if (!clean_name)
-		return FALSE;
-
-	while (blocklist)
-	{
-		if (!strcmp(blocklist->data, clean_name))
-			return TRUE;
-
-		blocklist = g_list_next(blocklist);
-	}
-	return FALSE;
-}
 
 static void
 msg_blocked_cb(PurpleAccount* account, char *sender)
@@ -210,7 +152,7 @@ receiving_im_msg_cb(PurpleAccount* account, char **sender, char **message,
 #endif // GLIB_CHECK_VERSION
 
 	// block blocked buddies
-	if (contact_is_blocked(*sender))
+	if (blocklist_contains(*sender))
 	{
 		purple_debug_info("pidgin-pp", "Blocking %s\n", *sender);
 
@@ -327,7 +269,7 @@ request_authorization_cb(PurpleAccount* account, char *sender)
 	}
 #endif // PURPLE_VERSION_CHECK
 
-	if (prefs_auth_block_repeated() && contact_is_blocked(sender))
+	if (prefs_auth_block_repeated() && blocklist_contains(sender))
 	{
 		return -1; // deny
 	}
@@ -351,8 +293,8 @@ authorization_deny_cb(PurpleAccount* account, char *sender)
 	purple_debug_info("pidgin-pp",
 		"Processing rejected authorization request from %s\n", sender);
 
-	if (!contact_is_blocked(sender))
-		add_to_block_list(sender);
+	if (!blocklist_contains(sender))
+		blocklist_add(sender);
 }
 
 static void
@@ -396,149 +338,14 @@ jabber_xmlnode_cb(PurpleConnection *gc, xmlnode **packet, gpointer null)
 	g_free (node_name);
 }
 
-static void
-block_contact_cb(PurpleBlistNode *node, gpointer data)
-{
-	const gchar* name = PURPLE_BLIST_NODE_NAME(node);
-	add_to_block_list(name);
-}
-
-static void
-unblock_contact_cb(PurpleBlistNode *node, gpointer data)
-{
-	const gchar *name = PURPLE_BLIST_NODE_NAME(node);
-	remove_from_block_list(name);
-}
-
-static void
-mouse_menu_cb(PurpleBlistNode *node, GList **menu)
-{
-	if (purple_blist_node_get_flags(node) & PURPLE_BLIST_NODE_FLAG_NO_SAVE)
-		return;
-
-	const gchar *contact_name = PURPLE_BLIST_NODE_NAME(node);
-
-	// don't show the contect menu on things other than contacts
-	if (!contact_name)
-		return;
-
-	PurpleMenuAction *action = NULL;
-
-	*menu = g_list_append(*menu, action);
-
-	//purple_debug_info("pidgin-pp", "Clicked on %s\n", contact_name);
-
-	if (contact_is_blocked(contact_name))
-	{
-		action = purple_menu_action_new(_("Unblock (privacy please)"),
-			PURPLE_CALLBACK(unblock_contact_cb), NULL, NULL);
-	}
-	else
-	{
-		action = purple_menu_action_new(_("Block (privacy please)"),
-			PURPLE_CALLBACK(block_contact_cb), NULL, NULL);
-	}
-
-	*menu = g_list_append(*menu, action);
-}
-
-static void
-del_button_clicked_cb(GtkWidget *widget, GtkTreeSelection *selection)
-{
-	GtkTreeIter iter;
-	GtkTreeModel *model;
-	GValue value;
-
-	memset(&value, 0, sizeof(GValue));
-
-	if (gtk_tree_selection_get_selected(selection, &model, &iter))
-	{
-		gtk_tree_model_get_value(model, &iter, 0, &value);
-		gchar *name = (gchar *) g_value_get_string (&value);
-		remove_from_block_list(name);
-
-		if (gtk_list_store_remove(GTK_LIST_STORE(model), &iter))
-		{
-			gtk_tree_selection_select_iter(selection, &iter);
-		}
-		g_value_unset(&value);
-	}
-}
-
-static void
-manage_blocked_users_cb(PurplePluginAction *action)
-{
-	GtkWidget *vbox, *window, *treeview;
-	GtkWidget *del_button;
-	GtkWidget *buttons;
-	GtkWidget *scrolled_window;
-	GtkListStore *store = gtk_list_store_new(1, G_TYPE_STRING);
-	GtkTreeIter iter;
-	GtkCellRenderer *cellrenderer;
-	GtkTreeViewColumn *list_column;
-	GtkTreeSelection *selection;
-	GList *blocklist;
-
-	blocklist = prefs_get_block_list();
-
-	while (blocklist)
-	{
-		gtk_list_store_append(store, &iter);
-		gtk_list_store_set(store, &iter, 0, blocklist->data, -1);
-		blocklist = g_list_next(blocklist);
-	}
-
-	window = pidgin_create_window(_("Privacy Please"),
-			PIDGIN_HIG_BORDER, NULL, TRUE);
-	gtk_window_set_default_size(GTK_WINDOW(window), 380, 200);
-
-	vbox = gtk_vbox_new(FALSE, 6);
-	gtk_container_set_border_width(GTK_CONTAINER(vbox), 4);
-	gtk_widget_show(vbox);
-
-	treeview = gtk_tree_view_new_with_model(GTK_TREE_MODEL(store));
-	gtk_widget_set_size_request(treeview, 200, 150);
-
-	cellrenderer = gtk_cell_renderer_text_new();
-	list_column = gtk_tree_view_column_new_with_attributes
-			(_("Blocked users"), cellrenderer, "text", 0, NULL);
-	gtk_tree_view_column_set_min_width(list_column, 300);
-	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), list_column);
-
-	scrolled_window = gtk_scrolled_window_new(NULL, NULL);
-	gtk_container_add(GTK_CONTAINER(scrolled_window), treeview);
-	gtk_scrolled_window_set_shadow_type(
-			GTK_SCROLLED_WINDOW(scrolled_window),
-			GTK_SHADOW_IN);
-	gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window),
-			GTK_POLICY_AUTOMATIC,
-			GTK_POLICY_AUTOMATIC);
-
-	buttons = gtk_hbox_new(FALSE, 0);
-
-	del_button = gtk_button_new_from_stock(GTK_STOCK_DELETE);
-	gtk_box_pack_end(GTK_BOX(buttons), del_button, FALSE, FALSE, 0);
-
-	selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(treeview));
-
-	gtk_container_add(GTK_CONTAINER(window), vbox);
-	gtk_container_add(GTK_CONTAINER(vbox), scrolled_window);
-	gtk_box_pack_start(GTK_BOX(vbox), buttons, FALSE, FALSE, 0);
-
-	gtk_widget_show_all(window);
-
-	g_signal_connect(GTK_OBJECT(del_button), "clicked",
-			GTK_SIGNAL_FUNC(del_button_clicked_cb), selection);
-}
-
 static GList *
 actions(PurplePlugin *plugin, gpointer context)
 {
 	GList *l = NULL;
 	PurplePluginAction *act = NULL;
 
-	act = purple_plugin_action_new(_("Manage blocked users"),
-			manage_blocked_users_cb);
+	act = purple_plugin_action_new(
+				_("Manage blocked users"), blocklist_manage);
 	l = g_list_append(l, act);
 
 	return l;
@@ -584,7 +391,7 @@ plugin_load (PurplePlugin * plugin)
 
 	purple_signal_connect(purple_blist_get_handle(),
 			"blist-node-extended-menu", plugin,
-			PURPLE_CALLBACK(mouse_menu_cb), NULL);
+			PURPLE_CALLBACK(blocklist_mouse_action), NULL);
 	return TRUE;
 }
 
